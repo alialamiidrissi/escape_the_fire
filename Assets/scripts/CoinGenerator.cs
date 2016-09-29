4611a1af8ae2986ab3954b6c2c47c646a4b4b3ce
alialamiidrissi <ali.alami-idrissi@epfl.ch>
2016-09-29 17:36:41 +0200

    Possibility to collect life Points added

--- Assets/scripts/CoinGenerator.cs
+++ Assets/scripts/CoinGenerator.cs
@@ -1,29 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CoinGenerator : MonoBehaviour {
+public class CoinGenerator : MonoBehaviour
+{
 
     public ObjectPool coins_pool;
+    public ObjectPool life_points;
     private int nbCoins;
+    public int lifePointsprobability;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         nbCoins = Random.Range(1, 3);
-	}
+    }
 
 
-    public void createCoins(Vector3 positionT,bool addCentral)
+    public void createCoins(Vector3 positionT, bool addCentral)
     {
         createCoin(new Vector3(positionT.x - 1.5f, positionT.y + 1f, positionT.z));
         if (nbCoins >= 2 && addCentral)
             createCoin(new Vector3(positionT.x, positionT.y + 1f, positionT.z));
         if (nbCoins == 3)
             createCoin(new Vector3(positionT.x + 1.5f, positionT.y + 1f, positionT.z));
-        nbCoins= Random.Range(1, 3);
+        nbCoins = Random.Range(1, 3);
     }
 
     private void createCoin(Vector3 pos)
     {
-        GameObject coin1 = coins_pool.getObject();
+        GameObject coin1;
+        if (Random.Range(0, 100) < lifePointsprobability)
+            coin1 = life_points.getObject();
+        else
+            coin1 = coins_pool.getObject();
         coin1.transform.position = pos;
         coins_pool.enableObject(coin1);
     }
---
+++ Assets/scripts/LifePointHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifePointHandler : MonoBehaviour {
+
+    // Use this for initialization
+    private ScoreLPManager lifePoints;
+    void Start()
+    {
+        lifePoints = FindObjectOfType<ScoreLPManager>();
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+
+    {
+        if (coll.gameObject.name == "player")
+        {
+            gameObject.SetActive(false);
+            lifePoints.increaseLifePoints(10);
+        }
+    }
+}
